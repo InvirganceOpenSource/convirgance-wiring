2ea302f6b0a12762e091132d843ae0b7aa27d7af
jbanes <jbanes@gmail.com>
2025-05-19 22:18:03 -0500

    Added @Documented to @Wiring annotation to include it in JavaDocs

--- src/main/java/com/invirgance/convirgance/wiring/annotation/Wiring.java
+++ src/main/java/com/invirgance/convirgance/wiring/annotation/Wiring.java
@@ -21,15 +21,13 @@ SOFTWARE.
  */
 package com.invirgance.convirgance.wiring.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  *
  * @author jbanes
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Wiring
